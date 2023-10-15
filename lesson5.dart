d912f253eb5a727c9e943a1e6a77977e05b83a5f
bbaana <kurmankozoevabaana@gmail.com>
2023-10-16 03:29:28 +0600

    first commit

---
+++ lesson5.dart
@@ -0,0 +1,190 @@
+void main(List<String> args) {
+  //2//
+  int a = 8;
+  if (a == 10) {
+    print('верно');
+  } else {
+    print('не верно');
+  }
+
+  int a1 = 0;
+  if (a1 == 0) {
+    print('верно');
+  } else {
+    print('не верно');
+  }
+
+  int a2 = 1;
+  if (a2 == 0) {
+    print('верно');
+  } else {
+    print('не верно');
+  }
+
+  int a3 = -3;
+  if (a3 == 0) {
+    print('верно');
+  } else {
+    print('не верно');
+  }
+
+//3//
+  int a4 = 9;
+  if (a4 > 0) {
+    print('верно');
+  } else {
+    print('не верно');
+  }
+
+  int a5 = 1;
+  if (a5 > 0) {
+    print('верно');
+  } else {
+    print('не верно');
+  }
+
+  int a6 = 0;
+  if (a6 > 0) {
+    print('верно');
+  } else {
+    print('не верно');
+  }
+
+  int a7 = -3;
+  if (a7 > 0) {
+    print('верно');
+  } else {
+    print('не верно');
+  }
+
+//4//
+  int a8 = 40;
+  if (a8 < 0) {
+    print('верно');
+  } else {
+    print('не верно');
+  }
+
+  int a9 = 1;
+  if (a9 < 0) {
+    print('верно');
+  } else {
+    print('не верно');
+  }
+
+  int aA = 0;
+  if (aA < 0) {
+    print('верно');
+  } else {
+    print('не верно');
+  }
+
+  int aB = -3;
+  if (aB < 0) {
+    print('верно');
+  } else {
+    print('не верно');
+  }
+
+  //5//
+
+  int aC = 88;
+  if (aC == 0) {
+    print('верно');
+  } else {
+    print('не верно');
+  }
+
+  int aD = 1;
+  if (aD == 0) {
+    print('верно');
+  } else {
+    print('не верно');
+  }
+
+  int aI = 0;
+  if (aI > 0) {
+    print('верно');
+  } else {
+    print('не верно');
+  }
+
+  int aF = -3;
+  if (aF > 0) {
+    print('верно');
+  } else {
+    print('не верно');
+  }
+
+  //6//
+  int aG = 9;
+  if (aG < 0) {
+    print('верно');
+  } else {
+    print('не верно');
+  }
+
+  int aH = 1;
+  if (aH == 0) {
+    print('верно');
+  } else {
+    print('не верно');
+  }
+
+  int aZ = 0;
+  if (aZ == 0) {
+    print('верно');
+  } else {
+    print('не верно');
+  }
+
+  int aY = -3;
+  if (aY < 0) {
+    print('верно');
+  } else {
+    print('не верно');
+  }
+
+  //7//
+  int aP = 99;
+  if (aP != 0) {
+    print('верно');
+  } else {
+    print('не верно');
+  }
+
+  int aO = 1;
+  if (aO != 0) {
+    print('верно');
+  } else {
+    print('не верно');
+  }
+
+  int aX = 0;
+  if (aX != 0) {
+    print('верно');
+  } else {
+    print('не верно');
+  }
+
+  int aQ = -3;
+  if (aQ != 0) {
+    print('верно');
+  } else {
+    print('не верно');
+  }
+
+  //8//
+  dynamic aV = 'test';
+  print(aV);
+  dynamic aK = '3';
+  print(aK);
+  dynamic aT = 'baiana';
+  print(aT);
+
+  //9)//
+  String aU = '7';
+  if (a.runtimeType == String) {
+    print('верно');
+  }
+}
